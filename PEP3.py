99967c20b157f226e69fdbac53194d200b3cb665
Markus Hatlo <marhat182@gmail.com>
2025-02-07 03:13:51 +0100

    rust jobbing

--- PEP3.py
+++ PEP3.py
@@ -14,11 +14,71 @@ import matplotlib.pyplot as plt
 
 # Q_needed = np.sum(Q)
 
-# if Q_needed > 0:
+
+hot_stream = {
+    "H1": {
+        "mCp": 50,
+        "InletT": 200,
+        "FinalT": 70,
+    },
+    "H2": {
+        "mCp": 10,
+        "InletT": 300,
+        "FinalT": 60,
+    }
+}
+cold_stream = {       
+    "C1": {
+        "mCp": 70,
+        "InletT": 90,
+        "FinalT": 180,
+    },
+    "C2": {
+        "mCp": 15,
+        "InletT": 40,
+        "FinalT": 220,
+    },
+}
 
 
+def calc_q(stream):
+    q_list = []
+    q_list_tot = []
+    for duty in stream.values(): 
+        t_diff = duty['InletT'] - duty['FinalT']
+        q = duty['mCp'] * t_diff
 
+        q_list.append(q)
 
+        q_tot = 0
+        for q_itr in q_list_tot:
+            q_tot += q_itr
+        q_tot += q
+
+        q_list_tot.append(q_tot)
+
+    return q_list, q_list_tot
+
+q_hot, q_hot_tot  = calc_q(hot_stream)
+q_cold, q_cold_tot  = calc_q(cold_stream)
+
+def get_temp_list(stream):
+    temp_list = []
+    for temp in stream.values():
+        temp_list.append(temp['FinalT'])
+    return temp_list
+
+hot_temps = get_temp_list(hot_stream)
+cold_temps = get_temp_list(cold_stream)
+
+print(q_hot,q_hot_tot,hot_temps)
+
+plt.figure(figsize=(8,8))
+
+plt.plot(q_hot_tot, hot_temps, marker='o', linestyle='-', color="purple", label="Hot Composite Curve")
+plt.plot(q_cold, cold_temps, marker='o', linestyle='-', color="blue", label="Cold Composite Curve")
+plt.plot(1000,100,color="white")
+plt.show()
 
 
 
