27ec27b20d01c382292d91c83d45f693ddfc8997
Markus Hatlo <marhat182@gmail.com>
2025-02-07 02:18:39 +0100

    first commit

---
+++ PEP3.py
@@ -0,0 +1,73 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# mCp = np.array([50,10,70,15])
+# T_in = np.array([200,300,90,40,30,240])
+# T_fin = np.array([70,60,180,220,40,240])
+# Q = []
+
+# for i in range(len(mCp)):
+#     Q.append(int(mCp[i] * (T_in[i] - T_fin[i])))
+
+# print(Q)
+
+# Q_needed = np.sum(Q)
+
+# if Q_needed > 0:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# H1 = [T_,200]
+# HQ1 = [0,Q[0]]
+# H2 = [10,300]
+# HQ2 = [HQ1[1],HQ1[1]+Q[1]]
+# C1 = []
+
+# plt.figure(figsize=(6,6))
+# plt.plot(20000,1,color="white")
+# plt.plot(HQ1,H1,color="red")
+# plt.plot(HQ2,H2,color="red")
+# plt.plot()
+
+# plt.show()
---
+++ PEP3Ex.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+hot_temps = [50, 60, 70, 90]
+hot_q = [0, 150, 233, 500]
+
+
+cold_temps = [10, 20, 30, 40, 60, 100]  
+cold_q = [0, 100, 225, 250, 333, 400]  
+
+
+plt.figure(figsize=(8,8))
+
+plt.plot(hot_q, hot_temps, marker='o', linestyle='-', color="purple", label="Hot Composite Curve")
+plt.plot(cold_q, cold_temps, marker='o', linestyle='-', color="blue", label="Cold Composite Curve")
+plt.plot(1000,100,color="white")
+
+
+plt.xlabel("Q [MJ/min]")
+plt.ylabel("Temp [°C]")
+plt.title("Composite Curves")
+plt.legend()
+plt.locator_params(axis='both', nbins=15)  # Increase the number of bins (more grid lines)
+plt.grid()
+#plt.minorticks_on()  # Enable minor ticks
+#plt.grid(True, which='minor', linestyle=':', linewidth=0.5)  # Minor grid (dotted, thinner)
+plt.show()
