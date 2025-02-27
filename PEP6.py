3a109b05375542bb163468b3b76d5b81152e2bf1
Markus Hatlo <marhat182@gmail.com>
2025-02-27 15:53:46 +0100

    Jobber med pep6

--- PEP6.py
+++ PEP6.py
@@ -1,3 +1,15 @@
 import numpy as np
+import scipy as sp
 
-print('Hello')
+
+# def results(Q11,Q12,Q21,Q22)
+    
+#     return Ccop, Cop, GHG, TAC
+
+# for guess in [1,1000]:
+#     Q = rand(4,1)
+#     Q_alternativ = sp.LatinHypercube(d=2)
+#     obj = results(Q)
+
+# plot
+
